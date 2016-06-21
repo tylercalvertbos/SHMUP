f3b4c0eaac236e7c5a40fcd6f2692da290708a1c
tylercalvertbos <studio.freelance@tbwamel-mac29.local>
2016-06-21 17:32:43 +1000

    a

--- js/game.js
+++ js/game.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
 	var canvas = document.createElement('canvas');
 	var ctx = canvas.getContext('2d');
 
-	canvas.width = 1225;
-	canvas.height = 674;
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
 
 	document.body.appendChild(canvas)
 
