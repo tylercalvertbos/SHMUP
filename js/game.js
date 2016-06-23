098c557874e0e0297a5ef393289a34afd82adf52
tylercalvertbos <studio.freelance@tbwamel-mac29.local>
2016-06-23 12:51:34 +1000

    a

--- js/game.js
+++ js/game.js
@@ -13,20 +13,16 @@ $(document).ready(function() {
 
 // Define Variables
 
-window.onload = function() {
-	var score = 0;
-}
-
-var lives = 3;
-var shots = [];
+var score = 0;
+var lives = 5;
 var keysDown = {};
+var difficulty = 2;
+var scoreIncrement;
+var enemySpeed;
 
 // Enemies
 
 var enemySprites = ['images/enemy4/enemy4.png', 'images/enemy1/enemy1.png', 'images/enemy2/enemy2.png', 'images/enemy3/enemy3.png']
-for (var i = 0 ; i < enemySprites.length ; i++) {
-	enemySprite.src = enemySprites[i];
-}
 
 // Background Image
 
@@ -58,16 +54,36 @@ enemySprite.onload = function() {
 	enemyReady = true;
 };
 
-for (var i = 0 ; i < enemySprites.length ; i++) {
-	enemySprite.src = enemySprites[i];
-}
+enemySprite.src = enemySprites[1];
+
+enemy2Sprite = new Image();
+enemy2Sprite.src = enemySprites[2];
+
+enemy3Sprite = new Image();
+enemy3Sprite.src = enemySprites[3];
+
+enemy4Sprite = new Image();
+enemy4Sprite.src = enemySprites[0];
+
+enemy5Sprite = new Image();
+enemy5Sprite.src = enemySprites[1];
+
+enemy6Sprite = new Image();
+enemy6Sprite.src = enemySprites[2];
+
+enemy7Sprite = new Image();
+enemy7Sprite.src = enemySprites[3];
+
+enemy8Sprite = new Image();
+enemy8Sprite.src = enemySprites[0]; 
+
 
 // Objects
 
 var hero = {
-	speed : 600,
+	speed : 1200,
 	x : 0,
-	y : 0,
+	y : canvas.height / 2.75,
 	width : 100,
 	height : 100
 }
@@ -80,17 +96,60 @@ var bullet = {
 }
 
 var enemy = {
-	x : 0,
+	x : window.innerWidth - hero.width,
 	y : 0
 }
+
+var enemy2 = {
+	x : window.innerWidth - hero.width,
+	y : 0
+}
+
+var enemy3 = {
+	x : window.innerWidth - hero.width,
+	y : 0
+}
+
+var enemy4 = {
+	x : window.innerWidth - hero.width,
+	y : 0
+}
+var enemy5 = {
+	x : window.innerWidth - hero.width,
+	y : 0
+}
+
+var enemy6 = {
+	x : window.innerWidth - hero.width,
+	y : 0
+}
+
+var enemy7 = {
+	x : window.innerWidth - hero.width,
+	y : 0
+}
+
+var enemy8 = {
+	x : window.innerWidth - hero.width,
+	y : 0
+}
+
+var enemyX = enemy.x;
+var enemyX2 = enemy2.x;
+var enemyX3 = enemy3.x;
+var enemyX4 = enemy4.x;
+var enemyX5 = enemy5.x;
+var enemyX6 = enemy6.x;
+var enemyX7 = enemy7.x;
+var enemyX8 = enemy8.x;
+
+var enemyYPositions = [enemy.y, enemy2.y, enemy3.y, enemy4.y]
 
 var eBullet = {
 	speed : 1500,
 	x : 0,
 	y : 0
 }
-// Pause
-
 
 // Player Input
 
@@ -120,7 +179,7 @@ var update = function (modifier) {
 
 	}
 	if (82 in keysDown) { // Player reloading
-		update();
+		location.reload();
 	}
 	if (80 in keysDown) { // Player pausing
 		score += 5867097;
@@ -139,33 +198,114 @@ var update = function (modifier) {
 		hero.y = canvas.height - hero.height;
 	}
 
-	if (hero.x < enemy.x + 100 && hero.x + 100 > enemy.x && hero.y < enemy.y + 100 && 100 + hero.y > enemy.y) {
+	if (hero.x < enemyX + 100 && hero.x + 100 > enemyX && hero.y < enemy.y + 100 && 100 + hero.y > enemy.y) {
 		lives -= 1;
 		reset();
 	}
 
+	if (hero.x < enemyX4 + 100 && hero.x + 100 > enemyX4 && hero.y < enemy4.y + 100 && 100 + hero.y > enemy4.y) {
+		lives -= 1;
+		reset();
+	}
+
+	if (hero.x < enemyX3 + 100 && hero.x + 100 > enemyX3 && hero.y < enemy3.y + 100 && 100 + hero.y > enemy3.y) {
+		lives -= 1;
+		reset();
+	}
+
+	if (hero.x < enemyX2 + 100 && hero.x + 100 > enemyX2 && hero.y < enemy2.y + 100 && 100 + hero.y > enemy2.y) {
+		lives -= 1;
+		reset();
+	}
+
+	if (hero.x < enemyX5 + 100 && hero.x + 100 > enemyX5 && hero.y < enemy5.y + 100 && 100 + hero.y > enemy5.y) {
+		lives -= 1;
+		reset();
+	}
+
+	if (hero.x < enemyX6 + 100 && hero.x + 100 > enemyX6 && hero.y < enemy6.y + 100 && 100 + hero.y > enemy6.y) {
+		lives -= 1;
+		reset();
+	}
+
+	if (hero.x < enemyX7 + 100 && hero.x + 100 > enemyX7 && hero.y < enemy7.y + 100 && 100 + hero.y > enemy7.y) {
+		lives -= 1;
+		reset();
+	}
+
+	if (hero.x < enemyX8 + 100 && hero.x + 100 > enemyX8 && hero.y < enemy8.y + 100 && 100 + hero.y > enemy8.y) {
+		lives -= 1;
+		reset();
+	}
+
+if (lives > 0 ) {
+	score += 10;
+}
+
 $('div').html('SCORE<br><br>' + score);
 $('#lives').html(lives + '<br><br>LIVES')
 
-
+if (lives == 0) {
+	alert('Your score was ' + score);
+	window.location = 'gameover.html';
 }
 
-// Score
+if (difficulty == 1) {
+	hero.speed = 256;
+	enemySpeed = 15;
+	scoreIncrement = 5;
+} else if (difficulty == 2) {
+	hero.speed = 512;
+	enemySpeed = 20;
+	scoreIncrement = 15;
+} else if (difficulty == 3) {
+	hero.speed = 720;
+	enemySpeed = 35;
+	scoreIncrement = 40;
+} else if (difficulty == 4) {
+	hero.speed = 1200;
+	enemySpeed = 43;
+	scoreIncrement = 50;
+}
 
+}
 
 // Reset Game
 
 var reset = function() {
-	hero.y = canvas.height / 2.75;
-	score = 0;
 
-	enemy.x = 0;
 	enemy.y = (Math.random() * window.innerHeight)
+	enemy2.y = (Math.random() * window.innerHeight)
+	enemy3.y = (Math.random() * window.innerHeight)
+	enemy4.y = (Math.random() * window.innerHeight)
+	enemy5.y = (Math.random() * window.innerHeight)
+	enemy6.y = (Math.random() * window.innerHeight)
+	enemy7.y = (Math.random() * window.innerHeight)
+	enemy8.y = (Math.random() * window.innerHeight)
+
+	enemyX = enemy.x;
+	enemyX2 = enemy2.x;
+	enemyX3 = enemy3.x;
+	enemyX4 = enemy4.x;
+	enemyX5 = enemy5.x;
+	enemyX6 = enemy6.x;
+	enemyX7 = enemy7.x;
+	enemyX8 = enemy8.x;
 }
 
 // Draw Stuff
 
 var drawStuff = function() {
+
+	enemyX = enemyX - enemySpeed;
+	enemyX2 = enemyX2 - enemySpeed;
+	enemyX3 = enemyX3 - enemySpeed;
+	enemyX4 = enemyX4 - enemySpeed;
+	enemyX5 = enemyX5 - enemySpeed;
+	enemyX6 = enemyX6 - enemySpeed;
+	enemyX7 = enemyX7 - enemySpeed;
+	enemyX8 = enemyX8 - enemySpeed;
+	
 	if (bgReady) {
 		ctx.drawImage(bgImage, 0, 0, window.innerWidth, window.innerHeight);
 	}
@@ -175,8 +315,19 @@ var drawStuff = function() {
 	}
 
 	if (enemyReady) {
-			ctx.drawImage(enemySprite, enemy.x, enemy.y, 100, 100)
+			ctx.drawImage(enemySprite, enemyX, enemy.y, 100, 100)
+			ctx.drawImage(enemy2Sprite, enemyX2, enemy2.y, 100, 100)
+			ctx.drawImage(enemy3Sprite, enemyX3, enemy3.y, 100, 100)
+			ctx.drawImage(enemy4Sprite, enemyX4, enemy4.y, 100, 100)
+			ctx.drawImage(enemy5Sprite, enemyX5, enemy5.y, 100, 100)
+			ctx.drawImage(enemy6Sprite, enemyX6, enemy6.y, 100, 100)
+			ctx.drawImage(enemy7Sprite, enemyX7, enemy7.y, 100, 100)
+			ctx.drawImage(enemy8Sprite, enemyX8, enemy8.y, 100, 100)
 		}
+
+	if (enemyX < 0) {
+		reset();
+	}
 	}
 
 
